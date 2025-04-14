3869d21c279e1ae0489f232f04037da6abf11eaa
zack6yuan <zack6yuan@gmail.com>
2025-04-13 20:27:18 -0500

    updating.

--- scripts.js
+++ scripts.js
@@ -1,22 +1,9 @@
-/* function getFirstQuote() {
-  fetch("https://smileschool-api.hbtn.info/quotes")
-  .then(response => response.json())
-  .then(data => {
-    const textData = document.getElementById("firstQuote")
-    textData.textContent = data.text;
-  })
-  .catch(error => {
-    console.error(error);
-  })
-}
-getFirstQuote(); */
-
 function setNameOne() {
   fetch("https://smileschool-api.hbtn.info/quotes")
   .then(response => response.json())
   .then(data => {
-    const textData = document.getElementById("nameOne")
-    textData.textContent = data[0].name;
+    const nameFetch = document.getElementById("nameOne")
+    nameFetch.textContent = data[0].name;
   })
   .catch(error => {
     console.error(error);
@@ -24,6 +11,38 @@ function setNameOne() {
 }
 setNameOne();
 
-function setJobOne() {
-  
+function setTitleOne() {
+  fetch("https://smileschool-api.hbtn.info/quotes")
+  .then(response => response.json())
+  .then(data => {
+    const titleFetch = document.getElementById("titleOne")
+    titleFetch.textContent = data[0].title;
+  })
+  .catch(error => {
+    console.error(error)
+  })
 }
+setTitleOne();
+
+function setNameTwo() {
+  fetch("https://smileschool-api.hbtn.info/quotes")
+  .then(response => response.json())
+  .then(data => {
+    const nameFetch = document.getElementById("nameTwo")
+    nameFetch.textContent = data[1].name;
+  })
+}
+setNameTwo();
+
+function setTitleTwo() {
+  fetch("https://smileschool-api.hbtn.info/quotes")
+  .then(response => response.json())
+  .then(data => {
+    const titleFetch = document.getElementById("titleTwo")
+    titleFetch.textContent = data[1].title;
+  })
+  .catch(error => {
+    console.error(error)
+  })
+}
+setTitleTwo();
